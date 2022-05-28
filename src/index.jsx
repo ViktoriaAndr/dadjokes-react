e6dee18fa77ef0458b6fd29aa9f6c3b831ba0a02
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-05-28 14:59:12 +0200

    vytvorenie komponenty Joke pred mapovanim

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { Joke } from './Joke';
+
+let upLikes = 0;
+let downLikes = 0;
+
+const buttonUp = () => {
+  upLikes += 1;
+  const likesUpElm = document.querySelector('#likes-up');
+  likesUpElm.textContent = upLikes;
+};
+
+const buttonDown = () => {
+  downLikes += 1;
+  const likesDownElm = document.querySelector('#likes-down');
+  likesDownElm.textContent = downLikes;
+};
 
 const App = () => (
   <div className="container">
-    <div className="joke">
-      <div className="joke__body">
-        <div className="joke__user">
-          <img
-            class="user-avatar"
-            src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-          />
-          <p className="user-name">Neroxx</p>
-        </div>
-
-        <p className="joke__text">
-          The secret service isn't allowed to yell "Get down!" anymore when the
-          president is about to be attacked. Now they have to yell "Donald,
-          duck!"
-        </p>
-      </div>
-      <div className="joke__likes">
-        <button id="btn-up" className="btn-like btn-like--up"></button>
-        <span id="likes-up" className="likes-count likes-count--up">
-          0
-        </span>
-        <button id="btn-down" className="btn-like btn-like--down"></button>
-        <span id="likes-down" className="likes-count likes-count--down">
-          0
-        </span>
-      </div>
-    </div>
+    <Joke />
   </div>
 );
 
